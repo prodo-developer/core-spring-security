a574c62f8c77990d385b941d23c50aa6ba0a4698
prodo-developer <wjdals300@gmail.com>
2022-02-16 23:40:33 +0900

    spring security 실습

---
+++ src/main/java/io/securitycorespringsecurity/controller/user/UserController.java
@@ -0,0 +1,15 @@
+package io.securitycorespringsecurity.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping(value="/mypage")
+    public String myPage() throws Exception {
+
+        return "user/mypage";
+    }
+}
