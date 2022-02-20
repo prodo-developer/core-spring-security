2e363be580f1c6a7f72be5ba264d5ea8796bef43
prodo-developer <wjdals300@gmail.com>
2022-02-20 09:47:37 +0900

    feat: DB연동 인증처리 공통모듈 생성(Service)

---
+++ src/main/java/io/security/corespringsecurity/security/service/CustomUserDetailService.java
@@ -0,0 +1,39 @@
+package io.security.corespringsecurity.security.service;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("userDetailsService")
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // 데이터계층으로 정보를 가져옴.
+        Account account = userRepository.findByUsername(username);
+
+        if(account == null) {
+            throw new UsernameNotFoundException("UsernameNotFoundException");
+        }
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(account.getRole()));
+
+        AccountContext accountContext = new AccountContext(account, roles);
+
+        return accountContext;
+    }
+}
