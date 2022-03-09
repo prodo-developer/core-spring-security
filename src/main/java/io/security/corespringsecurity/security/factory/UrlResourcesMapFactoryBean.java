6637539e82e075c34751da67e2c8cde116c46864
prodo-developer <wjdals300@gmail.com>
2022-03-09 23:54:37 +0900

    feat : db 리소스를 가져오기 위한작업

---
+++ src/main/java/io/security/corespringsecurity/security/factory/UrlResourcesMapFactoryBean.java
@@ -0,0 +1,48 @@
+package io.security.corespringsecurity.security.factory;
+
+import io.security.corespringsecurity.security.service.SecurityResourceService;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class UrlResourcesMapFactoryBean implements FactoryBean<LinkedHashMap<RequestMatcher, List<ConfigAttribute>>> {
+
+    // DB로 부터 가져오는 서비스
+    private SecurityResourceService securityResourceService;
+    // Bean으로 만들 resourceMap
+    private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> resourceMap;
+
+    public UrlResourcesMapFactoryBean(SecurityResourceService securityResourceService) {
+        this.securityResourceService = securityResourceService;
+    }
+
+    @Override
+    public LinkedHashMap<RequestMatcher, List<ConfigAttribute>> getObject() throws Exception {
+
+        // null 일경우 초기화
+        if(resourceMap == null) {
+            init();
+        }
+        
+        return resourceMap;
+    }
+
+    private void init() {
+        resourceMap = securityResourceService.getResourceList();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return LinkedHashMap.class;
+    }
+
+    // 싱글톤 (메모리에 하나만 존재)
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+}
---
+++ src/main/java/io/security/corespringsecurity/security/service/SecurityResourceService.java
@@ -0,0 +1,40 @@
+package io.security.corespringsecurity.security.service;
+
+import io.security.corespringsecurity.domain.entity.Resources;
+import io.security.corespringsecurity.repository.ResourcesRepository;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class SecurityResourceService {
+
+    private ResourcesRepository resourcesRepository;
+
+    public void setResourceRepository(ResourcesRepository resourcesRepository) {
+        this.resourcesRepository = resourcesRepository;
+    }
+
+    public LinkedHashMap<RequestMatcher, List<ConfigAttribute>> getResourceList() {
+
+        LinkedHashMap<RequestMatcher, List<ConfigAttribute>> result = new LinkedHashMap<>();
+        List<Resources> resourcesList = resourcesRepository.findAllResources(); // db정보를 가져옴
+        // 리소스를 돌때마다
+        resourcesList.forEach(re -> {
+            List<ConfigAttribute> configAttributeList = new ArrayList<>();
+            // 권한정보
+            re.getRoleSet().forEach(role -> {
+                configAttributeList.add(new SecurityConfig(role.getRoleName())); // db에서 가져온 role 정보
+                // 리소스 하나당 일 대다 구조로 매핑함
+                result.put(new AntPathRequestMatcher(re.getResourceName()), configAttributeList);
+            });
+
+        });
+
+        return result;
+    }
+}
