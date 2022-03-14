0fdc4a9010a47ff4ab66de852eee07a34526134f
prodo-developer <wjdals300@gmail.com>
2022-03-14 23:50:09 +0900

    refactor : 아이디 검증 비지니스 사용

--- src/main/java/io/security/corespringsecurity/security/service/UserDetailsServiceImpl.java
+++ src/main/java/io/security/corespringsecurity/security/service/UserDetailsServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,6 +27,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private HttpServletRequest request;
 
+    /**
+     * failed to lazily initialize a collection of role: io.security.corespringsecurity.domain.entity.Account.userRoles
+     * select 한 entitiy가 영속성 컨텍스트 내에 존재하지 않기 때문에 발생한 에러
+     * 원인 : 위 에러에서 말하는 세션이 바로 영속성 컨텍스트를 말하는 것이고, 이는 한 트랜잭션안에 해당 entitiy가 존재하지 않다는 것
+     * 해결책1 : 재 지연로딩으로 되어있는 연관관계를 즉시로딩으로 변경하여 한번에 가져오기
+     * 해결책2 : 메서드에 @Transactional 어노테이션을 줘서 트랜잭션 내에 존재
+     */
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Account account = userRepository.findByUsername(username);
