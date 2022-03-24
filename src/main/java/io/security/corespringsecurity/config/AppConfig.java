77f47ee012a6bdd22f03f7cf39221e0bb7e05fcd
prodo-developer <wjdals300@gmail.com>
2022-03-24 23:53:47 +0900

    feat : AccessIp 정보 가져오는 비지니스 로직 추가

--- src/main/java/io/security/corespringsecurity/config/AppConfig.java
+++ src/main/java/io/security/corespringsecurity/config/AppConfig.java
@@ -1,8 +1,9 @@
 package io.security.corespringsecurity.config;
 
+import io.security.corespringsecurity.repository.AccessIpRepository;
 import io.security.corespringsecurity.repository.ResourcesRepository;
 import io.security.corespringsecurity.security.configs.MethodSecurityConfig;
-import io.security.corespringsecurity.security.service.SecurityResourceService;
+import io.security.corespringsecurity.service.SecurityResourceService;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,8 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public SecurityResourceService securityResourceService(ResourcesRepository resourcesRepository) {
-        SecurityResourceService securityResourceService = new SecurityResourceService(resourcesRepository);
+    public SecurityResourceService securityResourceService(ResourcesRepository resourcesRepository, AccessIpRepository accessIpRepository) {
+        SecurityResourceService securityResourceService = new SecurityResourceService(resourcesRepository, accessIpRepository);
 
         return securityResourceService;
     }
